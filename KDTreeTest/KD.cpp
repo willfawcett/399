44fe079bfe3927da08e03f70908a8d9156bf0ef2
Will Fawcett <will.fawcett@gmail.com>
2017-11-26 16:37:33 -0600

    Branching
    Added some branching based on number of processors.  Runs parallel version with 1 proc, runs mpi version if 4 procs detected.

--- KDTreeTest/KD.cpp
+++ KDTreeTest/KD.cpp
@@ -33,6 +33,13 @@ void GenerateRandomTree(int nNodes)
 	}
 }
 
+void parallel_execution(int *numProcesses, int *rank) {
+	
+	//MPI 4 processer version goes here
+
+	return;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -50,71 +57,88 @@ int main(int argc, char* argv[])
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	// Send the random number generator to get different results each time for each processor
-	srand((int)time(NULL)*rank);
-	GenerateRandomTree(POINTS_NUM);
 
-	cout << "KD Tree Created by process " << rank << endl;
-	
-	// generate random point to query tree
-	int pTarget[SD];
-	float disKD;
-	bool loop = true;
-
-	while (loop) 
-	{
-
-		//input file is hard coded to keep I/O from affecting exicution time
-		ifstream points("blah.txt");
-
-		string point;
-
-		//put points in array
-		if (points.is_open())
-		{
-			int i = 0;
-			const char* fileName = "_NN";
-			const char* fileType = ".txt";
-			char name_buffer[512];
-			while (!points.eof())
-			{
-				//get line as string then cast into int array
-				getline(points, point);
-				//cout << "Point " << i << ": " << point << endl;
-				stringstream stream(point);
-				for (int j = 0; j < SD; j++)
-					stream >> pTarget[j];
-				
-				//generate correct file name
-				sprintf(name_buffer, "%d%s%s", i, fileName, fileType);
-				ofstream near_output(name_buffer);
-				
-				//put point in first line of file
-				near_output << point << endl;
-
-				//compute nearest and make it the second line in file
-				KDNode<int>* nearest = Tree.find_nearest(pTarget);
-				disKD = (float)sqrt(Tree.d_min);
-				//cout << "Nearest point for point " << i << ": ";
-				for (int i = 0; i < SD; i++)
-				{
-					//cout << nearest->x[i] << " ";
-					near_output << nearest->x[i] << " ";
-				}
-				//cout << endl;
-				near_output << endl;
-
-				//put discance in third line of file
-				//cout << "Distance: " << disKD << endl;
-				near_output << disKD;
-
-				i++;
-			}
-			points.close();
-			loop = false;
-		}
-		else cout << "Error: Could not open file.\n";
+	if (numProcesses == 4) {
+		parallel_execution(&numProcesses, &rank);
 	}
+	else if (numProcesses != 1) {
+		//We only support 1 or 4 processes
+		//Exit with error message
+		if(rank == 1)
+			fprintf(stderr, "Please try again, using either 1 or 4 processes.");
+		MPI_Barrier(MPI_COMM_WORLD); //synch processes
+		MPI_Finalize(); //exit MPI gracefully
+		exit(1); //end app prematurely
+	}
+	else {
+		// SERIAL VERSION
+
+		// Send the random number generator to get different results each time for each processor
+		srand((int)time(NULL)*rank);
+		GenerateRandomTree(POINTS_NUM);
+
+		cout << "KD Tree Created by process " << rank << endl;
+
+		// generate random point to query tree
+		int pTarget[SD];
+		float disKD;
+		bool loop = true;
+
+		while (loop)
+		{
+
+			//input file is hard coded to keep I/O from affecting exicution time
+			ifstream points("blah.txt");
+
+			string point;
+
+			//put points in array
+			if (points.is_open())
+			{
+				int i = 0;
+				const char* fileName = "_NN";
+				const char* fileType = ".txt";
+				char name_buffer[512];
+				while (!points.eof())
+				{
+					//get line as string then cast into int array
+					getline(points, point);
+					//cout << "Point " << i << ": " << point << endl;
+					stringstream stream(point);
+					for (int j = 0; j < SD; j++)
+						stream >> pTarget[j];
+
+					//generate correct file name
+					sprintf(name_buffer, "%d%s%s", i, fileName, fileType);
+					ofstream near_output(name_buffer);
+
+					//put point in first line of file
+					near_output << point << endl;
+
+					//compute nearest and make it the second line in file
+					KDNode<int>* nearest = Tree.find_nearest(pTarget);
+					disKD = (float)sqrt(Tree.d_min);
+					//cout << "Nearest point for point " << i << ": ";
+					for (int i = 0; i < SD; i++)
+					{
+						//cout << nearest->x[i] << " ";
+						near_output << nearest->x[i] << " ";
+					}
+					//cout << endl;
+					near_output << endl;
+
+					//put discance in third line of file
+					//cout << "Distance: " << disKD << endl;
+					near_output << disKD;
+
+					i++;
+				}
+				points.close();
+				loop = false;
+			}
+			else cout << "Error: Could not open file.\n";
+		} // end while loop
+	}// END if/elseif/else
 
 	//display exicution time
 	clock_t end = clock();
